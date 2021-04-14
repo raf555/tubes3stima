7e5b380437803762582e71b991edbec6a27e4dee
Glitch (tubes3stima) <none>
2021-04-14 14:43:08 +0000

    rapihin + minor fix

--- router.js
+++ router.js
@@ -19,82 +19,95 @@ app.get("/parsechat", (req, res) => {
 });
 
 function process(parsed) {
-  let status, response;
+  let result;
   const taskdb = editJsonFile("db/task.json");
   let type = parsed.type;
   if (type == "add") {
-    // add task type
-    let id = (Object.keys(taskdb.get()).length + 1).toString();
-    let data = {
-      tanggal: makedate(parsed.tanggal),
-      kuliah: parsed.kuliah,
-      jenis: parsed.task,
-      topik: parsed.konten,
-      selesai: false
-    };
-    taskdb.set(id, data);
-    taskdb.save();
-
-    status = "ok";
-    response = `[TASK BERHASIL DICATAT]\n(ID: ${id}) ${data.tanggal} - ${data.kuliah} - ${data.jenis} - ${data.topik}`;
+    result = add(taskdb, parsed);
   } else if (type == "update") {
-    // update date
-    let id = parsed.id;
-    let newdate = parsed.newdate;
-    let newdatestamp = new Date(
-      `${newdate.bln}/${newdate.tgl}/${newdate.thn}`
-    ).getTime();
-    let indikator = parsed.indikator;
-
-    let target = taskdb.get(id);
-
-    if (!target) {
-      status = "error";
-      response = "Task dengan id " + id + " tidak ditemukan.";
-    } else {
-      let curdate = target.tanggal.split("/");
-      curdate = new Date(`${curdate[1]}/${curdate[0]}/${curdate[2]}`).getTime();
-      newdate = makedate(newdate);
-
-      if (indikator == "diubah") {
-        taskdb.set(id + ".tanggal", newdate);
-        taskdb.save();
-        status = "ok";
-        response = "Task dengan id " + id + " berhasil diperbarui tanggalnya.";
-      } else if (indikator == "diundur") {
-        if (newdatestamp < curdate) {
-          status = "error";
-          response = "Tanggal tidak valid";
-        } else {
-          taskdb.set(id + ".tanggal", newdate);
-          taskdb.save();
-          status = "ok";
-          response =
-            "Task dengan id " + id + " berhasil diperbarui tanggalnya.";
-        }
-      } else if (indikator == "dimajuin") {
-        if (newdatestamp > curdate) {
-          status = "error";
-          response = "Tanggal tidak valid";
-        } else {
-          taskdb.set(id + ".tanggal", newdate);
-          taskdb.save();
-          status = "ok";
-          response =
-            "Task dengan id " + id + " berhasil diperbarui tanggalnya.";
-        }
-      }
-    }
+    result = update(taskdb, parsed);
   } else if (type == "finish") {
   } else if (type == "fetch") {
   } else if (type == "find") {
   } else if (type == "help") {
   }
 
-  return {
+  return result;
+}
+
+function add(taskdb, parsed) {
+  let result, status, response;
+  // add task type
+  let id = (Object.keys(taskdb.get()).length + 1).toString();
+  let data = {
+    tanggal: makedate(parsed.tanggal),
+    kuliah: parsed.kuliah,
+    jenis: parsed.task,
+    topik: parsed.konten,
+    selesai: false
+  };
+  taskdb.set(id, data);
+  taskdb.save();
+
+  status = "ok";
+  response = `[TASK BERHASIL DICATAT]\n(ID: ${id}) ${data.tanggal} - ${data.kuliah} - ${data.jenis} - ${data.topik}`;
+
+  result = {
     status: status,
     response: response
   };
+}
+
+function update(taskdb, parsed) {
+  let result, status, response;
+
+  // update date
+  let id = parsed.id;
+  let newdate = parsed.newdate;
+  let newdatestamp = new Date(
+    `${newdate.bln}/${newdate.tgl}/${newdate.thn}`
+  ).getTime();
+  let indikator = parsed.indikator;
+
+  let target = taskdb.get(id);
+
+  if (!target) {
+    status = "error";
+    response = "Task dengan id " + id + " tidak ditemukan.";
+  } else {
+    let curdate = target.tanggal.split("/");
+    curdate = new Date(`${curdate[1]}/${curdate[0]}/${curdate[2]}`).getTime();
+    newdate = makedate(newdate);
+
+    if (indikator == "diubah") {
+      taskdb.set(id + ".tanggal", newdate);
+      taskdb.save();
+      status = "ok";
+      response = "Task dengan id " + id + " berhasil diperbarui tanggalnya.";
+    } else {
+      let condition;
+      if (indikator == "diundur") {
+        condition = newdatestamp < curdate;
+      } else if (indikator == "dimajuin") {
+        condition = newdatestamp > curdate;
+      }
+      if (!condition) {
+        status = "error";
+        response = "Tanggal tidak valid";
+      } else {
+        taskdb.set(id + ".tanggal", newdate);
+        taskdb.save();
+        status = "ok";
+        response = "Task dengan id " + id + " berhasil diperbarui tanggalnya.";
+      }
+    }
+  }
+  result = {
+    status: status,
+    response: response
+  };
+
+  return result;
 }
 
 function makedate(date) {
